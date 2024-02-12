1feb4220904623d6df7e18fd705cfb2d5eb808ed
emeriego@gmail.com <86321701+Emeriego@users.noreply.github.com>
2024-02-12 17:51:31 +0100

    first commit 3

--- chat/views.py
+++ chat/views.py
@@ -1,3 +1,12 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.http import HttpResponse
 
 # Create your views here.
+def index(request):
+    return render(request, "chat/index.html")
+
+def room(request):
+    pass
+
+def messages(request):
+    pass
